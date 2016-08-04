d70d4c5749e92885af264582ab3542428a9e373a
pastak <pasta0915@gmail.com>
2016-08-04 17:07:32 +0900

    Edge have chrome object but it is NOT for API usage

--- src/libs/polyfill.js
+++ src/libs/polyfill.js
@@ -1,7 +1,8 @@
 'use strict'
 module.exports = () => {
   try {
-    global.browser = global.chrome = global.chrome || global.browser
+    // Edge has chrome object but only chrome.app so Object which has tabs method is real API object.
+    global.browser = global.chrome = (global.chrome.tabs && global.chrome) || (global.browser.tabs && global.browser)
   } catch (e) {
     let global = window
     global.browser = global.chrome = global.chrome || global.browser
--- test/libs/polyfill.js
+++ test/libs/polyfill.js
@@ -15,33 +15,33 @@ test.afterEach((t) => {
 })
 
 test('has no `chrome`', (t) => {
-  window.browser = {}
+  window.browser = {tabs: {}}
   t.is(typeof window.chrome, 'undefined')
   polyfill()
-  t.is(typeof window.chrome, 'object')
+  t.is(typeof window.chrome.tabs, 'object')
 })
 
 test('has no `browser`', (t) => {
-  window.chrome = {}
+  window.chrome = {tabs: {}}
   t.is(typeof window.browser, 'undefined')
   polyfill()
-  t.is(typeof window.browser, 'object')
+  t.is(typeof window.browser.tabs, 'object')
 })
 
 test('has no `chrome` without `global`', (t) => {
   window = {}
-  window.browser = {}
+  window.browser = {tabs: {}}
   global = undefined
   polyfill()
-  t.is(typeof window.browser, 'object')
-  t.is(typeof window.chrome, 'object')
+  t.is(typeof window.browser.tabs, 'object')
+  t.is(typeof window.chrome.tabs, 'object')
 })
 
 test('has no `browser` without `global`', (t) => {
   window = {}
-  window.chrome = {}
+  window.chrome = {tabs: {}}
   global = undefined
   polyfill()
-  t.is(typeof window.browser, 'object')
-  t.is(typeof window.chrome, 'object')
+  t.is(typeof window.browser.tabs, 'object')
+  t.is(typeof window.chrome.tabs, 'object')
 })
