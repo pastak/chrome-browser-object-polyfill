15a5af8d5c7a6332964ff9593afdd7189ec7db1c
pastak <pasta0915@gmail.com>
2017-04-10 18:33:39 +0900

    fix for MS Edge

--- src/libs/polyfill.js
+++ src/libs/polyfill.js
@@ -2,7 +2,7 @@
 module.exports = () => {
   try {
     // Edge has chrome object but only chrome.app so Object which has tabs method is real API object.
-    global.browser = global.chrome = (global.chrome.tabs && global.chrome) || (global.browser.tabs && global.browser)
+    global.browser = global.chrome = (global.chrome && global.chrome.tabs && global.chrome) || (global.browser && global.browser.tabs && global.browser)
   } catch (e) {
     let global = window
     global.browser = global.chrome = global.chrome || global.browser
